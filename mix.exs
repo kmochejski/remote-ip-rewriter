fcfc608c8960fe3172f1e784de923c8da19b95a6
Krzysztof Mochejski <krzysztofmo@wp.pl>
2016-04-15 10:29:15 +0200

    Version bump to 0.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule RemoteIpRewriter.Mixfile do
 
   def project do
     [app: :remote_ip_rewriter,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
